faf8a397097f03c5fff685237c68b23294b806b8
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-03-04 09:48:06 +0300

    added constructor code

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,11 @@ var player2="";
 var throwdice = function () {
   return Math.floor(6*Math.random())+1;
 }
+
+function Player(turn){
+  this.roll = 0;
+  this.roundscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.playerName;
+}
